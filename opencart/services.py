236ac62403f4f32a4fcf05d15791a00526b21ccd
Sergey Smirnov <smirnoffs@gmail.com>
2015-08-16 17:37:41 +0300

    test for get_last_success_import added

--- opencart/services.py
+++ opencart/services.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Sergey Smirnov <smirnoffs@gmail.com>'
 
+import datetime
+
 import pymongo
+
 from karsender.config import Config
 
-client = pymongo.MongoClient(Config.MONGO_DB_HOST)
-db = client[Config.MONGO_DB_DATABASE]
-collection= db[Config.ORDERS_COLLECTION]
+
+def get_collection():
+    client = pymongo.MongoClient(Config.MONGO_DB_HOST)
+    db = client[Config.MONGO_DB_DATABASE]
+    return db[Config.ORDERS_COLLECTION]
+
 
 def get_last_success_import():
-    last_import = collection.find_one(success=True).sort('datetime')
+    last_import = get_collection().find({'success': True}).sort('datetime', pymongo.DESCENDING).limit(1)
+    if not last_import.count():
+        return datetime.datetime(year=1980, month=1, day=1)
+    return last_import[0]['datetime']
 
 
 def get_last_orders():
+    last_import = get_last_success_import()
--- opencart/tests.py
+++ opencart/tests.py
@@ -1,12 +1,44 @@
 # -*- coding: utf-8 -*-
+import datetime
+from unittest.mock import patch
+
+from karsender import Config
+
+from opencart.services import get_last_success_import, get_collection
+
 __author__ = 'Sergey Smirnov <smirnoffs@gmail.com>'
 
 import unittest
 
 
+class MockConfig(Config):
+    ORDERS_COLLECTION = 'test_collection'
+
+
+mock = patch.object(Config, 'ORDERS_COLLECTION', 'test_collection')
+
+
 class TestOrderExport(unittest.TestCase):
-    def test_get_last_orders(self):
-        orders = get_last_orders()
-        self.assertIsNotNone(orders)
-        self.assertGreater(len(orders), 0)
-        self.assertTrue(all(isinstance(order, Order) for order in orders))
+    # def test_get_last_orders(self):
+    #     orders = get_last_orders()
+    #     self.assertIsNotNone(orders)
+    #     self.assertGreater(len(orders), 0)
+    #     self.assertTrue(all(isinstance(order, Order) for order in orders))
+
+    def setUp(self):
+        mock.start()
+        self.collection = get_collection()
+
+    def test_get_last_success_import(self):
+        last_import = get_last_success_import()
+        self.assertIsInstance(last_import, datetime.datetime)
+        self.assertEqual(last_import, datetime.datetime(year=1980, month=1, day=1))
+        success_date = datetime.datetime(year=2014, month=2, day=12, hour=12, minute=4, second=14)
+        self.collection.insert_one({'success': True, 'datetime': success_date})
+        self.collection.insert_one({'success': False, 'datetime': datetime.datetime.now()})
+        self.assertIsInstance(last_import, datetime.datetime)
+        self.assertEqual(get_last_success_import(), success_date)
+
+    def tearDown(self):
+        self.collection.drop()
+        mock.stop()
